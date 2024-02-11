ad36853a17f559b4e94968e8eef1fcb6a2b197c3
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-02-11 13:30:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+let x=document.getElementById("sizeInfo");
+let head=document.createElement('h1');
+	window.onresize = length;
+	  window.onload = length;	
+	x.appendChild(head);
+	function length() {
+	let width=window.outerWidth;
+	let height=window.outerHeight;
+		head.textContent=`Width:${width} and Height: ${height}`;
+
+
+
+
